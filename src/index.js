3e841aaea6787f5b6c2e89d87106f2500c822222
Bogdan Ryzhko <m3meizumini@gmail.com>
2023-01-30 15:09:23 +0200

    add libraries lodash.debounce and notiflix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const country = document.getElementById('search-box');
+const link = `https://restcountries.com/v3.1/name/${country}`;
+
+function showCountries(event) {
+	let countryName = event.target.value;
+	console.log(countryName);
+}
+
+country.addEventListener('input', debounce(showCountries, DEBOUNCE_DELAY));
